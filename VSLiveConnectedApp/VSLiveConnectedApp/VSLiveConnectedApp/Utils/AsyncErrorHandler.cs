abe875afedd1beb257c3e9a178b65a1f37911f62
Roy Cornelissen <rcornelissen@xpirit.com>
2015-11-15 16:32:09 +0100

    AsyncErrorHandler.Fody for global async errors
    Use AsyncErrorHandler.Fody to globally deal with async errors

---
+++ VSLiveConnectedApp/VSLiveConnectedApp/VSLiveConnectedApp/Utils/AsyncErrorHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace VSLiveConnectedApp.Utils
+{
+    public static class AsyncErrorHandler
+    {
+        public static void HandleException(Exception exception)
+        {
+            Debug.WriteLine(exception.Message);
+
+            // TODO: handle the exception in your own way, e.g. reporting it to
+            // an online application crash analytics service
+        }
+    }
+}
